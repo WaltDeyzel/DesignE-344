02ac322429c0448e20300ea45e4c7556308c2e42
21750793 <21750793@sun.ac.za>
2020-10-06 10:07:43 +0200

    why no working

--- Ass3/Assignment3/student_heart.py
+++ Ass3/Assignment3/student_heart.py
@@ -6,13 +6,6 @@ def calibrate(time, amplitude):
     # Enter calibration code here:
     ######################################
     bpm = 0
-    rise = 0
-    for beat in amplitude:
-        if(rise == 0 and beat > 4.5):
-            print('alive')
-            rise = 1
-        elif(rise == 1 and beat < 0.5):
-            rise = 0
         
         
     ######################################
--- Ass3/Assignment3/student_temp.py
+++ Ass3/Assignment3/student_temp.py
@@ -5,19 +5,8 @@ def calibrate(time, amplitude):
     ######################################
     # Enter calibration code here:
     ######################################
-
-    calibration_number = 4.77 # average of max analog reading at 42 degrees
-    minTemp = 34             # min temp for testing
-    div = 8                  # div between min and max temp 
-
-    # average analog value
-    analog = sum(amplitude)/len(amplitude)
-    # convertion formula
-    temperature = minTemp+analog*div/calibration_number
-
-
-    print(analog, temperature)
-
+    temperature = 0    
+        
         
     ######################################
         
